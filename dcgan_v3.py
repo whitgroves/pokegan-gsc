fdcddecd273f8896f951da56df8cd3af4a8d304d
whitgroves <whitney.groves@gmail.com>
2023-12-31 15:00:33 -0600

    v3 fixed

--- dcgan_v3.py
+++ dcgan_v3.py
@@ -5,62 +5,66 @@ import matplotlib.pyplot as plt
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # ignore bugged CUDA errors; must precede tf import
 import tensorflow as tf
 keras = tf.keras # autocomplete workaround for lazy-loading
-from keras import preprocessing, layers, losses, optimizers
+from keras import preprocessing, layers, losses, optimizers, Sequential
 if len(tf.config.list_physical_devices('GPU')) > 0:
     keras.mixed_precision.set_global_policy("mixed_float16") # NVIDIA speed optimization
+from IPython import display
 
-DATA_ROOT = './.data'
-RGB_SCALE = 255
-N_CHANNELS = 4 # rgba
-BATCH_SIZE = 32
-CONV_KERNEL =  (5, 5)
-CHKPT_DIR = './.checkpoints'
-EPOCHS = 50
+IMG_CHANNELS = 4 # rgba
+IMG_SCALE = 128
+BATCH_SIZE = 256 # default = 32
 NOISE_DIM = 100
-N_SAMPLES = 16
-GEN_SEED = tf.random.normal([N_SAMPLES, NOISE_DIM])
+CONV_KERNEL =  (5, 5)
+EPOCHS = 50
 
 def load_dataset(image_dir:str|os.PathLike) -> tf.data.Dataset:
-    dataset = preprocessing.image_dataset_from_directory(image_dir, label_mode=None, color_mode='rgba',
-                                                         image_size=(RGB_SCALE, RGB_SCALE))
-    return dataset.map(lambda image : image / RGB_SCALE) # normalize
+    dataset = preprocessing.image_dataset_from_directory(image_dir, label_mode=None,
+                                                         color_mode=('rgba' if IMG_CHANNELS == 4 else 'rgb'),
+                                                         image_size=(IMG_SCALE, IMG_SCALE),
+                                                         batch_size=BATCH_SIZE)
+    dataset = dataset.map(lambda image : image / 255) # [0, 255] -> [0, 1]
+    return dataset
 
 # wraps tf.data.Dataset since it doesn't seem to work with indexing or next()
-def get_sample(dataset:tf.data.Dataset, display=True) -> np.ndarray:
+def get_sample(dataset:tf.data.Dataset, display:bool=True) -> np.ndarray:
     for image in dataset:
-        image = (image.numpy() * RGB_SCALE).astype("uint8")[0] # rescale
+        image = (image.numpy() * 255).astype("uint8")[0] # [0, 1] -> [0, 255]
         if display:
             plt.axis('off')
             plt.imshow(image)
         yield image
 
-def make_generator() -> keras.Sequential:
-    return keras.Sequential([
-        layers.Dense(N_SAMPLES*N_SAMPLES*(RGB_SCALE+1), use_bias=False, input_shape=(NOISE_DIM,)),
+def make_generator() -> Sequential:
+    seed_scale = IMG_SCALE // 4 # used for the initial dim so final image is correct scale
+    seed_channels = IMG_CHANNELS * 256 # not actually channels but upscaled to maintain total array size
+    model = Sequential([
+        layers.Dense(seed_scale**2 * seed_channels, use_bias=False, input_shape=(NOISE_DIM,)),
         layers.BatchNormalization(),
         layers.LeakyReLU(),
-        layers.Reshape((N_SAMPLES, N_SAMPLES, (RGB_SCALE+1))),
-        layers.Conv2DTranspose(128*N_CHANNELS, CONV_KERNEL, strides=(1, 1), padding='same', use_bias=False),
+        layers.Reshape((seed_scale, seed_scale, seed_channels)),
+        layers.Conv2DTranspose(128 * IMG_CHANNELS, CONV_KERNEL, strides=(1, 1), padding='same', use_bias=False),
         layers.BatchNormalization(),
         layers.LeakyReLU(),
-        layers.Conv2DTranspose(64*N_CHANNELS, CONV_KERNEL, strides=(2, 2), padding='same', use_bias=False),
+        layers.Conv2DTranspose(64 * IMG_CHANNELS, CONV_KERNEL, strides=(2, 2), padding='same', use_bias=False),
         layers.BatchNormalization(),
         layers.LeakyReLU(),
-        layers.Conv2DTranspose(N_CHANNELS, CONV_KERNEL, strides=(2, 2), padding='same', use_bias=False, activation='tanh')
+        layers.Conv2DTranspose(IMG_CHANNELS, CONV_KERNEL, strides=(2, 2), padding='same', use_bias=False, activation='tanh')
     ])
+    assert model.output_shape == (None, IMG_SCALE, IMG_SCALE, IMG_CHANNELS)
+    return model
 
-def make_discriminator() -> keras.Sequential:
-    input_shape = (RGB_SCALE, RGB_SCALE, N_CHANNELS) # in v2, this was passed in
-    return keras.Sequential([
-        layers.Conv2D(64*N_CHANNELS, CONV_KERNEL, strides=(2, 2), padding='same', input_shape=input_shape),
+def make_discriminator() -> Sequential:
+    model = Sequential([
+        layers.Conv2D(64 * IMG_CHANNELS, CONV_KERNEL, strides=(2, 2), padding='same', input_shape=[IMG_SCALE, IMG_SCALE, IMG_CHANNELS]),
         layers.LeakyReLU(),
         layers.Dropout(0.3),
-        layers.Conv2D(128*N_CHANNELS, CONV_KERNEL, strides=(2, 2), padding='same'),
+        layers.Conv2D(128 * IMG_CHANNELS, CONV_KERNEL, strides=(2, 2), padding='same'),
         layers.LeakyReLU(),
         layers.Dropout(0.3),
         layers.Flatten(),
         layers.Dense(1)
     ])
+    return model
 
 # helper function to compute model loss
 cross_entropy = losses.BinaryCrossentropy(from_logits=True)
@@ -75,13 +79,13 @@ def discriminator_loss(real:tf.Tensor, fake:tf.Tensor) -> tf.Tensor:
 generator_optimizer = optimizers.Adam(1e-4)
 discriminator_optimizer = optimizers.Adam(1e-4)
 
-def make_checkpoint(generator:keras.Sequential, discriminator:keras.Sequential) -> tf.train.Checkpoint:
+def make_checkpoint(generator:Sequential, discriminator:Sequential) -> tf.train.Checkpoint:
     return tf.train.Checkpoint(generator_optimizer=generator_optimizer,
                                discriminator_optimizer=discriminator_optimizer,
                                generator=generator, discriminator=discriminator)
 
 @tf.function # auto-compile
-def train_step(generator:keras.Sequential, discriminator:keras.Sequential, images:tf.Tensor) -> None:
+def train_step(generator:Sequential, discriminator:Sequential, images:tf.Tensor) -> None:
     noise = tf.random.normal([BATCH_SIZE, NOISE_DIM])
     with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
         gen_images = generator(noise, training=True)
@@ -94,24 +98,27 @@ def train_step(generator:keras.Sequential, discriminator:keras.Sequential, image
     generator_optimizer.apply_gradients(zip(gen_gradient, generator.trainable_variables))
     discriminator_optimizer.apply_gradients(zip(disc_gradient, discriminator.trainable_variables))
 
-def generate_and_save_images(generator:keras.Sequential, label:str) -> None:
-    images = generator(GEN_SEED, training=False)
+def generate_and_save_images(generator:Sequential, label:str) -> None:
+    out_dir = '.generated/'
+    if not os.path.exists(out_dir): os.makedirs(out_dir)
+    images = generator(tf.random.normal([16, NOISE_DIM]), training=False)
     _ = plt.figure(figsize=(4, 4)) # TODO: genrate plot dims based on N_SAMPLES
     for i in range(images.shape[0]):
         plt.subplot(4, 4, i+1)
-        plt.imshow(images[i, :, :, 0] * RGB_SCALE)
+        plt.imshow(images[i, :, :, 0] * IMG_SCALE)
         plt.axis('off')
-    plt.savefig(f'{label}.png')
+    plt.savefig(f'{out_dir}{label}.png')
     plt.show()
 
-def train(generator:keras.Sequential, discriminator:keras.Sequential, dataset:tf.data.Dataset) -> None:
+def train(generator:Sequential, discriminator:Sequential, dataset:tf.data.Dataset) -> None:
     for epoch in range(EPOCHS):
         msg = f'Epoch {epoch+1}/{EPOCHS}:'
         print(f'{msg} Training...', end='\r')
         start = time.time()
         for batch in dataset:
             train_step(generator, discriminator, batch)
+        display.clear_output(wait=True)
         generate_and_save_images(generator, epoch)
-        if epoch % 10 == 0: make_checkpoint(generator, discriminator).save()
+        if epoch % 10 == 0: make_checkpoint(generator, discriminator).save('.tf_checkpoints/')
         print(f'{msg} Complete in {time.time()-start:.1f}s')
     generate_and_save_images(generator, 'final')
