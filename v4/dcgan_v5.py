31d00df83dda8c7e96ba1908bfca073aa1cf2c02
whitgroves <whitney.groves@gmail.com>
2024-01-04 21:31:53 -0600

    v5 fail

--- v4/dcgan_v5.py
+++ v4/dcgan_v5.py
@@ -1,7 +1,6 @@
-# Special thanks: https://www.kaggle.com/code/cybersimar08/generate-realistic-human-face-using-gan
 import os
 import time
-import numpy as np
+from IPython import display
 import matplotlib.pyplot as plt
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # ignore bugged CUDA errors; must precede tf import
 import tensorflow as tf
@@ -11,12 +10,15 @@ if len(tf.config.list_physical_devices('GPU')) > 0:
     keras.mixed_precision.set_global_policy("mixed_float16") # NVIDIA speed optimization
 else: raise SystemError('No GPU detected. Please reload NVIDIA kernel.') # CPU overheats
 keras.utils.set_random_seed(1996)
-from IPython import display
 
 IMG_CHANNELS = 4 # rgba
 IMG_SCALE = 128 # 256 causes OOM
-BATCH_SIZE = 64 # default = 32
+IMG_SHAPE = [IMG_SCALE, IMG_SCALE, IMG_CHANNELS]
+BATCH_SIZE = 16 # default = 32
 NOISE_DIM = 151
+NOISE = tf.random.normal([BATCH_SIZE, NOISE_DIM])
+GRID_SIZE = int(BATCH_SIZE**0.5)
+plt.rc('figure', figsize=(GRID_SIZE, GRID_SIZE), autolayout=True)
 
 def load_dataset(image_dir:str|os.PathLike) -> tf.data.Dataset:
     dataset = utils.image_dataset_from_directory(image_dir, label_mode=None,
@@ -27,7 +29,7 @@ def load_dataset(image_dir:str|os.PathLike) -> tf.data.Dataset:
     return dataset
 
 # displays a `size` x `size` grid of pictures from `images`
-def display_grid(images:tf.Tensor, size:int=4) -> None:
+def display_grid(images:tf.Tensor, size:int=GRID_SIZE) -> None:
     for i in range(size**2):
         image = (images[i].numpy() * 255).astype('uint8') # [0, 1] -> [0, 255]
         plt.subplot(size, size, i+1)
@@ -59,14 +61,13 @@ def make_generator() -> Sequential:
         layers.LeakyReLU(),
         layers.Conv2DTranspose(IMG_CHANNELS, 5, strides=2, padding='same', use_bias=False, activation='sigmoid')
     ])
-    if model.output_shape != (None, IMG_SCALE, IMG_SCALE, IMG_CHANNELS):
-        raise ValueError(f'Model output has wrong shape: {model.output_shape}')
+    if model.output_shape != (None, *IMG_SHAPE): raise ValueError(f'Wrong output shape: {model.output_shape}')
     model.compile(get_optimizer(), loss='binary_crossentropy')
     return model
 
 def make_discriminator() -> Sequential:
     model = Sequential([
-        layers.Conv2D(64 * IMG_CHANNELS, 5, strides=2, padding='same', input_shape=[IMG_SCALE, IMG_SCALE, IMG_CHANNELS]),
+        layers.Conv2D(64 * IMG_CHANNELS, 5, strides=2, padding='same', input_shape=IMG_SHAPE),
         layers.LeakyReLU(),
         layers.Dropout(0.3),
         layers.Conv2D(128 * IMG_CHANNELS, 5, strides=2, padding='same'),
@@ -77,11 +78,10 @@ def make_discriminator() -> Sequential:
         layers.Dense(1, activation='sigmoid')
     ])
     model.compile(get_optimizer(), loss='binary_crossentropy')
-    model.trainable = False
     return model
 
 # helper function to compute model loss
-cross_entropy = losses.BinaryCrossentropy() #from_logits=True) # <- not needed with sigmoid
+cross_entropy = losses.BinaryCrossentropy() # add `from_logits=True` if switching back to tanh
 
 def generator_loss(fake:tf.Tensor) -> tf.Tensor:
     return cross_entropy(tf.ones_like(fake), fake)
@@ -90,12 +90,12 @@ def discriminator_loss(real:tf.Tensor, fake:tf.Tensor) -> tf.Tensor:
     return cross_entropy(tf.ones_like(real), real) + cross_entropy(tf.zeros_like(fake), fake)
 
 # these need to persist
-generator_optimizer = get_optimizer()
-discriminator_optimizer = get_optimizer()
+G_OPTIMIZER = get_optimizer()
+D_OPTIMIZER = get_optimizer()
 
 def make_checkpoint(generator:Sequential, discriminator:Sequential) -> tf.train.Checkpoint:
-    return tf.train.Checkpoint(generator_optimizer=generator_optimizer,
-                               discriminator_optimizer=discriminator_optimizer,
+    return tf.train.Checkpoint(generator_optimizer=G_OPTIMIZER,
+                               discriminator_optimizer=D_OPTIMIZER,
                                generator=generator, discriminator=discriminator)
 
 @tf.function # auto-compile
@@ -109,15 +109,15 @@ def train_step(generator:Sequential, discriminator:Sequential, images:tf.Tensor)
         disc_loss = discriminator_loss(disc_real, disc_fake)
     gen_gradient = gen_tape.gradient(gen_loss, generator.trainable_variables)
     disc_gradient = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
-    generator_optimizer.apply_gradients(zip(gen_gradient, generator.trainable_variables))
-    discriminator_optimizer.apply_gradients(zip(disc_gradient, discriminator.trainable_variables))
+    G_OPTIMIZER.apply_gradients(zip(gen_gradient, generator.trainable_variables))
+    D_OPTIMIZER.apply_gradients(zip(disc_gradient, discriminator.trainable_variables))
 
 def generate_and_save_images(generator:Sequential, label:str) -> None:
     out_dir = '.generated/'
     if not os.path.exists(out_dir): os.makedirs(out_dir)
     display.clear_output(wait=True)
-    images = generator(tf.random.normal([4**2, NOISE_DIM]), training=False)
-    display_grid(images, 4)
+    images = generator(NOISE, training=False)
+    display_grid(images)
     plt.savefig(f'{out_dir}{label}.png')
     plt.show()
 
@@ -134,7 +134,9 @@ def train(generator:Sequential, discriminator:Sequential, dataset:tf.data.Datase
         start = time.time()
         for batch in dataset: train_step(generator, discriminator, batch)
         generate_and_save_images(generator, f'{int(timestamp)}_{epoch}') # timestamp == run ID
-        if (epoch+1) % 10 == 0: checkpoint_mgr.save() # save every 10 epochs
+        if (epoch+1) % 10 == 0: # save every 10 epochs
+            print(f'{msg} Saving...', end='\r')
+            checkpoint_mgr.save() 
         print(f'{msg} Complete in {time.time()-start:.1f}s')
     generate_and_save_images(generator, 'final')
     print(f'{epochs} epochs completed in {time.time()-timestamp:.1f}s')
